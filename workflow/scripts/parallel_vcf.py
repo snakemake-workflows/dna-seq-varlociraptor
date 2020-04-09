18a7e60b28d4754245dd97955b3206fc000a0e83
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2020-04-09 11:07:00 +0000

    some changes fpr parallel_vcf

--- workflow/scripts/parallel_vcf.py
+++ workflow/scripts/parallel_vcf.py
@@ -10,36 +10,36 @@ parser.add_argument("file_vcf")
 parser.add_argument("command")
 parser.add_argument("--stepsize", "-s", type=int, default=1000000)
 parser.add_argument("--threads", "-t", type=int, default=8)
-parser.add_argument("--tmp", default=None, help="Define the base directory for the tmp files")
+parser.add_argument(
+    "--tmp", default=None, help="Define the base directory for the tmp files"
+)
 args = parser.parse_args()
 tmp_base = args.tmp
 stepsize = args.stepsize
 
 # create regions
-f = VariantFile(args.file_vcf)
-regions = []
-for name, contig in f.header.contigs.items():
-    last = 0
-    for x in list(range(0, contig.length, stepsize)):
-        stop = min(last + stepsize - 1, contig.length)
-        regions.append(f"{name}:{last}-{stop}")
-        last = stop+1
-f.close() 
+with VariantFile(args.file_vcf) as f:
+    regions = []
+    for name, contig in f.header.contigs.items():
+        last = 0
+        for x in list(range(0, contig.length, stepsize)):
+            stop = min(last + stepsize - 1, contig.length)
+            regions.append(f"{name}:{last}-{stop}")
+            last = stop + 1
 
-#command = "SnpSift annotate {ss_args} {args.database_vcf} /dev/stdin"
+# command = "SnpSift annotate {ss_args} {args.database_vcf} /dev/stdin"
 command = args.command
 
-with tempfile.TemporaryDirectory(dir=tmp_base) as t:
-    def f(x):
-        #print("start", x, file=sys.stderr)
-        cmd = f"bcftools view -r {x} {args.file_vcf} | bcftools view -t {x} | {command} | bcftools view -Ob > {t}/{x}.bcf"
+with tempfile.TemporaryDirectory(dir=tmp_base) as tmp_dir:
+
+    def execute_partial(x):
+        cmd = f"bcftools view -r {x} {args.file_vcf} -Ou | bcftools view -t {x} | {command} | bcftools view -Ob > {t}/{x}.bcf"
         os.system(cmd)
-        #print("done", x, file=sys.stderr)
-        return f"{t}/{x}.bcf"
+        return os.path.join(tmp_dir, f"{x}.bcf")
 
     with Pool(args.threads) as p:
         first = True
-        for filename in p.imap(f, regions):
+        for filename in p.imap(execute_partial, regions):
             print("merge", filename, file=sys.stderr)
             if first:
                 cmd = f"bcftools view {filename}"
