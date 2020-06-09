31b7706d2cc2782375ebf14033c2e4ad5384f079
Felix Mölder <felix.moelder@uni-due.de>
2020-06-09 12:46:18 +0200

    Minor changes

--- workflow/scripts/build_target_regions.py
+++ workflow/scripts/build_target_regions.py
@@ -1,26 +1,25 @@
 import pandas as pd
 
-log_file = open(snakemake.log[0], "w")
-out = open(snakemake.output[0], "w")
-
 chunksize = 10 ** 6
-for data_primers in pd.read_csv(
-    snakemake.input[0],
-    sep="\t",
-    header=None,
-    chunksize=chunksize,
-    usecols=[0, 1, 3, 5, 6],
-):
-    valid_primers = data_primers[0] == data_primers[3]
-    print(
-        data_primers[valid_primers]
-        .drop(columns=[3, 6])
-        .to_csv(sep="\t", index=False, header=False),
-        file=out,
-    )
-    print(
-        data_primers[~valid_primers].to_csv(sep="\t", index=False, header=False),
-        file=log_file,
-    )
-log_file.close()
-out.close()
+with open(snakemake.output[0], "w") as out:
+    with open(snakemake.log[0], "w") as log_file:
+        for data_primers in pd.read_csv(
+            snakemake.input[0],
+            sep="\t",
+            header=None,
+            chunksize=chunksize,
+            usecols=[0, 1, 3, 5, 6],
+        ):
+            valid_primers = data_primers[0] == data_primers[3]
+            print(
+                data_primers[valid_primers]
+                .drop(columns=[3, 6])
+                .to_csv(sep="\t", index=False, header=False),
+                file=out,
+            )
+            print(
+                data_primers[~valid_primers].to_csv(
+                    sep="\t", index=False, header=False
+                ),
+                file=log_file,
+            )
