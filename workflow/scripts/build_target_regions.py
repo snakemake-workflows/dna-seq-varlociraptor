2392efed69acea1d44186e8f702d13d9053e0405
Felix Mölder <felix.moelder@uni-due.de>
2020-06-09 10:19:03 +0200

    Parsing using pandas

--- workflow/scripts/build_target_regions.py
+++ workflow/scripts/build_target_regions.py
@@ -1,20 +1,24 @@
+import pandas as pd
+
 log_file = open(snakemake.log[0], "w")
 out = open(snakemake.output[0], "w")
 
-with open(snakemake.input[0], "r") as primer_file:
-    for line in primer_file.readlines():
-        line = line.strip().split("\t")
-        chr_1 = line[0]
-        chr_2 = line[3]
-        if chr_1 == chr_2:
-            print(
-                "{chr}\t{start}\t{end}".format(chr=chr_1, start=line[1], end=line[5]),
-                file=out,
-            )
-        else:
-            print(
-                "{primer}\t{chr1}\t{chr2}".format(
-                    primer=line[6], chr1=chr_1, chr2=chr_2
-                ),
-                file=log_file,
-            )
+chunksize = 10 ** 6
+for data_primers in pd.read_csv(
+    snakemake.input[0],
+    sep="\t",
+    header=None,
+    chunksize=chunksize,
+    usecols=[0, 1, 3, 5, 6],
+):
+    valid_primers = data_primers[0] == data_primers[3]
+    print(
+        data_primers[valid_primers]
+        .drop(columns=[3, 6])
+        .to_csv(sep="\t", index=False, header=False),
+        file=out,
+    )
+    print(
+        data_primers[~valid_primers].to_csv(sep="\t", index=False, header=False),
+        file=log_file,
+    )
