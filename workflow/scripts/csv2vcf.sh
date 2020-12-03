670f28f1e9fafbec5e17db4deb2a079a81f6f928
kilpert <fkilpert@gmail.com>
2020-12-03 16:40:10 +0100

    update csv2vcf.sh

--- workflow/scripts/csv2vcf.sh
+++ workflow/scripts/csv2vcf.sh
@@ -4,8 +4,10 @@
 ## Converts Varvis candidate variants (.csv) to vcf
 ## As of today (2020-11-27) Varvis export uses hg19!!!
 
-bname=$(basename ${1%.csv})
-person_id=$(echo $bname | tr "_" "\t" | cut -f1)
+upload_id=$(basename ${1%.tsv})
+upload_id_fix=$(echo $upload_id | tr "_" "-")
+##person_id=$(echo $upload_id | tr "_" "\t" | cut -f1)
+##echo $person_id
 
 ##tempdir="$(dirname $2)/temp"
 ##[ -d $tempdir ] || mkdir -p $tempdir
@@ -13,25 +15,25 @@ person_id=$(echo $bname | tr "_" "\t" | cut -f1)
 tempdir=$(mktemp -d)
 echo "TEMPDIR: $tempdir"
 
-## csv to tsv
-cat $1 | dos2unix | sed 's/,/\t/g' | sed 's/^\t/NA\t/; :a;s/\t\(\t\|$\)/\tNA\1/;ta' > $tempdir/${bname}.tsv 
+# csv to tsv
+#cat $1 | dos2unix | sed 's/,/\t/g' | sed 's/^\t/NA\t/; :a;s/\t\(\t\|$\)/\tNA\1/;ta' > $tempdir/${upload_id}.tsv 
 
 ## map
-tail -n+2 $tempdir/${bname}.tsv \
+tail -n+2 "$1" \
     | awk 'BEGIN{OFS="\t"}{id=sprintf("%s_%s_%s_%s_%s", $3, $4, $5, $6, $7); print $3, id, 0, $4}' \
     | sed 's/^XY/25/' \
     | sed 's/^MT/26/' \
     | sed 's/^X/23/' \
     | sed 's/Y/24/' \
-    > $tempdir/${bname}.map
+    > $tempdir/${upload_id}.map
 
 ## ped
-tail -n+2 $tempdir/${bname}.tsv | awk -v person_id=$person_id 'BEGIN{OFS=" "; aggr=""}{aggr=aggr " " $6 " " $7}END{print person_id, person_id, 0, 0, 0, -9 aggr}' > $tempdir/${bname}.ped
+tail -n+2 "$1" | awk -v upload_id_fix=$upload_id_fix 'BEGIN{OFS=" "; aggr=""}{aggr=aggr " " $6 " " $7}END{print upload_id_fix, upload_id_fix, 0, 0, 0, -9 aggr}' > $tempdir/${upload_id}.ped
 
-# convert map + ped to vcf
-plink --file $tempdir/$bname --recode vcf --out $tempdir/$bname
+## convert map + ped to vcf
+plink --file $tempdir/$upload_id --recode vcf --out $tempdir/$upload_id
 
-cat $tempdir/${bname}.vcf \
+cat $tempdir/${upload_id}.vcf \
     | sed 's/^23/X/' \
     | sed 's/^24/Y/' \
     | sed 's/^25/XY/' \
@@ -40,8 +42,8 @@ cat $tempdir/${bname}.vcf \
     | sed 's/##contig=<ID=24,/##contig=<ID=Y,/' \
     | sed 's/##contig=<ID=25,/##contig=<ID=XY,/' \
     | sed 's/##contig=<ID=26,/##contig=<ID=MT,/' \
-    > $tempdir/${bname}.temp.vcf
+    > $tempdir/${upload_id}.temp.vcf
 
-mv $tempdir/${bname}.temp.vcf $2
+mv $tempdir/${upload_id}.temp.vcf "$2"
 
 rm -r $tempdir
