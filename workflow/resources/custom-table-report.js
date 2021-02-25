0798b92efdfbc6e6284b4bd62992d4ef7149917e
Felix Mölder <felix.moelder@uni-due.de>
2021-02-25 11:18:44 +0100

    Add MetaKB Linkout

--- workflow/resources/custom-table-report.js
+++ workflow/resources/custom-table-report.js
@@ -44,6 +44,7 @@ $(document).ready(function () {
             $('#Button'+ j).append('<a class="dropdown-item" href="https://varsome.com/gene/'+ gene +'" target="_blank">VarSome</a>');
             $('#Button'+ j).append('<a class="dropdown-item" href="https://pandrugs.org/#!/query/?genes='+ gene +'" target="_blank">PanDrugs</a>');
             $('#Button'+ j).append('<a class="dropdown-item" href="https://www.mycancergenome.org/content/gene/'+ gene +'" target="_blank">My Cancer Genome</a>');
+            $('#Button'+ j).append('<a class="dropdown-item" href="https://search.cancervariants.org/#'+ gene +'" target="_blank">MetaKB</a>');
         }
         $('#ann-sidebar').append('</tr>');
 
