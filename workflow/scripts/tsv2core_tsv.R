ee127cb557f3b020af45cb2c4010e71f489288a0
Fabian Kilpert <fkilpert@gmail.com>
2021-03-18 13:17:22 +0000

    add tsv2core_tsv.R

---
+++ workflow/scripts/tsv2core_tsv.R
@@ -0,0 +1,21 @@
+#!/usr/bin/env Rscript
+
+library(readr)
+library(tidyverse)
+
+
+args = commandArgs(trailingOnly=TRUE)
+##args = c("~/vol/huge/exome_pools/dna-seq-varlociraptor/results/candidates/hg19/M55774.hg19_candidates.tsv",
+##         "~/vol/huge/exome_pools/dna-seq-varlociraptor/results/candidates/hg19/M55774.hg19_candidates.core.tsv")
+infile = args[[1]]
+outfile = args[[2]]
+
+d = read_tsv(infile)
+head(d)
+dim(d)
+
+d2 = d %>%
+  select(Chr, Start, End, Ref, Alt)
+
+write_tsv(d2, outfile, col_names=F)
+
