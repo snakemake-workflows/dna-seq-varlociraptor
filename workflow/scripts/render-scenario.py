2929e365b8ceb6fb3c8dc18f862e2c085b8685be
David Laehnemann <david.laehnemann@hhu.de>
2022-02-02 13:01:20 +0100

    try parquet instead of pickle, render-scenario.py

--- workflow/scripts/render-scenario.py
+++ workflow/scripts/render-scenario.py
@@ -1,12 +1,13 @@
 import sys
 from jinja2 import Template
 import pandas as pd
+import fastparquet
 
 sys.stderr = open(snakemake.log[0], "w")
 
 
 with open(snakemake.input[0]) as template, open(snakemake.output[0], "w") as out:
-    samples = snakemake.params.samples
+    samples = pd.read_parquet(snakemake.params.samples)
     out.write(
         Template(template.read()).render(
             samples=samples[samples["group"] == snakemake.wildcards.group]
