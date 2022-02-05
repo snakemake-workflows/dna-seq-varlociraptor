571cf551bdd081b3f131a503a2ac887014b1e2f2
Johannes Köster <johannes.koester@tu-dortmund.de>
2022-02-05 08:56:29 +0100

    adapt script

--- workflow/scripts/render-scenario.py
+++ workflow/scripts/render-scenario.py
@@ -1,13 +1,12 @@
 import sys
 from jinja2 import Template
 import pandas as pd
-import fastparquet
 
 sys.stderr = open(snakemake.log[0], "w")
 
 
 with open(snakemake.input[0]) as template, open(snakemake.output[0], "w") as out:
-    samples = pd.read_parquet(snakemake.params.samples)
+    samples = snakemake.params.samples
     out.write(
         Template(template.read()).render(
             samples=samples[samples["group"] == snakemake.wildcards.group]
