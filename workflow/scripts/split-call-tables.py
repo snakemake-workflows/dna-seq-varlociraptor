7d677973c278f23a67aa245c14e0b060089df8c1
Felix Mölder <felix.moelder@uni-due.de>
2022-03-03 14:57:56 +0100

    drop index

--- workflow/scripts/split-call-tables.py
+++ workflow/scripts/split-call-tables.py
@@ -6,6 +6,6 @@ calls = pd.read_csv(snakemake.input[0], sep="\t")
 
 coding = ~pd.isna(calls["hgvsp"])
 
-calls[~coding].dropna(how="all").to_csv(snakemake.output.noncoding)
+calls[~coding].dropna(how="all").to_csv(snakemake.output.noncoding, index=False)
 
-calls[coding].dropna(how="all").to_csv(snakemake.output.coding)
+calls[coding].dropna(how="all").to_csv(snakemake.output.coding, index=False)
