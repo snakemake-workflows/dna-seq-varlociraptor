0ec58475d9afdd208441a3a6657fd1fb2dd6d9ae
Felix Mölder <felix.moelder@uni-due.de>
2022-03-03 14:40:00 +0100

    remove function

--- workflow/scripts/split-call-tables.py
+++ workflow/scripts/split-call-tables.py
@@ -6,6 +6,6 @@ calls = pd.read_csv(snakemake.input[0], sep="\t")
 
 coding = ~pd.isna(calls["hgvsp"])
 
-retain_notna_cols(calls[~coding].dropna(how="all")).to_csv(snakemake.output.noncoding)
+calls[~coding].dropna(how="all").to_csv(snakemake.output.noncoding)
 
-retain_notna_cols(calls[coding].dropna(how="all")).to_csv(snakemake.output.coding)
+calls[coding].dropna(how="all").to_csv(snakemake.output.coding)
