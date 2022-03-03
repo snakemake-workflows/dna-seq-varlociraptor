01cda5779b99d9d06c266c2bb7eec2d9bdee5123
Felix Mölder <felix.moelder@uni-due.de>
2022-03-03 14:37:51 +0100

    Drop empty rows

--- workflow/scripts/split-call-tables.py
+++ workflow/scripts/split-call-tables.py
@@ -1,15 +1,11 @@
 import pandas as pd
 
 
-def retain_notna_cols(df):
-    return df.loc[:, df.notna().sum(axis="rows")]
-
-
 calls = pd.read_csv(snakemake.input[0], sep="\t")
 
 
 coding = ~pd.isna(calls["hgvsp"])
 
-retain_notna_cols(calls[~coding]).to_csv(snakemake.output.noncoding)
+retain_notna_cols(calls[~coding].dropna(how="all")).to_csv(snakemake.output.noncoding)
 
-retain_notna_cols(calls[coding]).to_csv(snakemake.output.coding)
+retain_notna_cols(calls[coding].dropna(how="all")).to_csv(snakemake.output.coding)
