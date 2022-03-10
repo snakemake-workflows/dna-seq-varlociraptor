fc36c2c2bead7aca4a85bae552223b5772272743
Felix Mölder <felix.moelder@uni-due.de>
2022-03-10 14:11:53 +0100

    Format table entries

--- workflow/scripts/split-call-tables.py
+++ workflow/scripts/split-call-tables.py
@@ -1,4 +1,6 @@
+import re
 import sys
+import numpy as np
 import pandas as pd
 
 sys.stderr = open(snakemake.log[0], "w")
@@ -10,7 +12,25 @@ def write(df, path):
     )
 
 
+def format_floats(df):
+    for col_name in df:
+        if issubclass(df[col_name].dtype.type, np.floating):
+            df[col_name] = [
+                "{:.2e}".format(x) if x < 0.1 and x > 0 else round(x, 2)
+                for x in df[col_name]
+            ]
+    return df
+
+
+def trim_hgvsp_entries(df):
+    if "hgvsp" in df.columns:
+        df["hgvsp"] = df["hgvsp"].apply(lambda x: re.sub(r"^.*?:", "", x) if pd.notnull(x) else x)
+    return df
+
+
 calls = pd.read_csv(snakemake.input[0], sep="\t")
+calls = format_floats(calls)
+calls = trim_hgvsp_entries(calls)
 calls.set_index("gene", inplace=True, drop=False)
 
 
@@ -37,5 +57,4 @@ write(
     ),
     snakemake.output.coding,
 )
-
 # TODO add possibility to also see non-canonical transcripts (low priority, once everything else works).
