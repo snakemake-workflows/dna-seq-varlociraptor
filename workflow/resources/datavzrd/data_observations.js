99a1ff3ed89a70a0e199983edbc35bdbbe53481b
Felix Mölder <felix.moelder@uni-due.de>
2023-03-22 11:52:29 +0100

    basic varlociraptor 7.0 support

--- workflow/resources/datavzrd/data_observations.js
+++ workflow/resources/datavzrd/data_observations.js
@@ -1,5 +1,5 @@
 function(value) {
-  var regex = /([0-9]+)(N|E|B|P|S|V|n|e|b|p|s|v)(s|p)(\#|\*|\.)(\+|\-|\*)(\>|\<|\*|\!)(\^|\*)(\$|\.)(\*|\.)/g;
+  var regex = /([0-9]+)(A|a|R|r)(N|E|B|P|S|V|n|e|b|p|s|v)(s|p)(\#|\*|\.)(\+|\-|\*)(\>|\<|\*|\!)(\^|\*)(\$|\.)(\*|\.)/g;
   var effects = {
       "N": "None",
       "E": "Equal",
@@ -15,20 +15,26 @@ function(value) {
       "<": "F2R1",
       "!": "non standard"
   }
+  var allele = {
+    "A": "Alt",
+    "R": "Ref"
+  }
+
   while ((result = regex.exec(value)) != null) {
-    strand = result[5].replace("*", "±")
-    effect = effects[result[2].toUpperCase()]
+    strand = result[6].replace("*", "±")
+    effect = effects[result[3].toUpperCase()]
     var quality = "Low mapping quality";
-    if (result[2] == result[2].toUpperCase()) {
+    if (result[3] == result[3].toUpperCase()) {
         quality = "High mapping quality";
     }
     observations.push({
+        "allele": allele[result[2].toUpperCase()],
         "strand": strand,
-        "strand_orientation": strand + ' ' + orientation[result[6]],
+        "strand_orientation": strand + ' ' + orientation[result[7]],
         "effect": effect,
         "times": parseFloat(result[1]),
         "quality": quality,
-        "orientation": orientation[result[6]]
+        "orientation": orientation[result[7]]
     })
   }
   return observations
--- workflow/resources/datavzrd/data_short_observations.js
+++ workflow/resources/datavzrd/data_short_observations.js
@@ -1,5 +1,5 @@
 function(value) {
-  var regex = /([0-9]+)(N|E|B|P|S|V|n|e|b|p|s|v)/g;
+  var regex = /([0-9]+)(A|a|R|r)(N|E|B|P|S|V|n|e|b|p|s|v)/g;
   var effects = {
       "N": "None",
       "E": "Equal",
@@ -8,14 +8,20 @@ function(value) {
       "S": "Strong",
       "V": "Very Strong"
   }
+  var allele = {
+    "A": "Alt",
+    "R": "Ref"
+  }
+
   var observations = [];
   while ((result = regex.exec(value)) != null) {
-    effect = effects[result[2].toUpperCase()]
+    effect = effects[result[3].toUpperCase()]
     var quality = "Low mapping quality";
-    if (result[2] == result[2].toUpperCase()) {
+    if (result[3] == result[3].toUpperCase()) {
         quality = "High mapping quality";
     }
     observations.push({
+        "allele": allele[result[2].toUpperCase()],
         "effect": effect,
         "times": parseFloat(result[1]),
         "quality": quality
