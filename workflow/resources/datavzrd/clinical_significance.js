9f6944140d4da45001105a505449cf15f99bcfa6
Felix Mölder <felix.moelder@uk-essen.de>
2024-05-23 08:41:15 +0200

    adjust coloring

--- workflow/resources/datavzrd/clinical_significance.js
+++ workflow/resources/datavzrd/clinical_significance.js
@@ -3,7 +3,7 @@ function clin_sig(value) {
     "unknown": "#6c757d", // gray
     "not_provided": "#343a40", // dark gray
     "other": "#adb5bd", // light gray
-    "benign": "#28a745", // green
+    "benign": "#218838", // green
     "benign/likely_benign": "#218838", // dark green
     "likely_benign": "#28a745", // green
     "protective": "#20c997", // teal
@@ -13,11 +13,11 @@ function clin_sig(value) {
     "affects": "#6f42c1", // purple
     "drug_response": "#6610f2", // dark purple
     "risk_factor": "#e3342f", // red
-    "likely_pathogenic": "#bd2130", // dark red
-    "pathogenic/likely_pathogenic": "#dc3545", // deep red
+    "likely_pathogenic": "#dc3545", // dark red
+    "pathogenic/likely_pathogenic": "#bd2130", // deep red
     "pathogenic": "#bd2130" // dark red
   };
   
-  const splitValues = value.split(",").map(item => `<span style="background-color: ${colorMapping[item.trim()]}"class="badge">${item.trim()}</span>`);
+  const splitValues = value.split(",").map(item => `<span style="color: white; background-color: ${colorMapping[item.trim()]}"class="badge">${item.trim()}</span>`);
   return splitValues.join(' ');  
 }
