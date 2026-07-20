b4b32e50d0eae6709f51d275d5a06098b9a56684
Johannes Koester <johannes.koester@uni-due.de>
2026-07-20 19:19:59 +0200

    fix

--- workflow/scripts/somalier_extract.py
+++ workflow/scripts/somalier_extract.py
@@ -1,17 +1,22 @@
+import os
 import tempfile
 from snakemake.shell import shell
 
-with tempfile.NamedTemporaryFile(dir="/dev/shm", suffix=".bam") as microbam:
-    print(microbam)
+with tempfile.NamedTemporaryFile(dir="/dev/shm", suffix=".bam", delete=False) as microbam:
+    microbam_path = microbam.name
+
+try:
     # use samtools to constrain input bam, it can read faster on non SSD (network, HDD)
     shell(
         "samtools view -u -M -L "
         "<(zcat {snakemake.input.sites} | awk '!/^#/{{print $1\"\\t\"$2-1\"\\t\"$2}}') "
-        "-o {microbam} {snakemake.input.bam} 2> {snakemake.log}"
+        "-o {microbam_path} {snakemake.input.bam} 2> {snakemake.log}"
     )
-    shell("samtools index {microbam} 2>> {log}")
+    shell("samtools index {microbam_path} 2>> {log}")
     shell(
         "somalier extract -d {snakemake.params.outdir} "
         "--sites {snakemake.input.sites} --sample-prefix {snakemake.wildcards.sample} "
-        "-f {snakemake.input.ref} {microbam} 2>> {log}"
+        "-f {snakemake.input.ref} {microbam_path} 2>> {log}"
     )
+finally:
+    os.remove(microbam_path)
