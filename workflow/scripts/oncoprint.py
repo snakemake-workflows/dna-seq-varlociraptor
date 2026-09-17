d73285de624fcd9f3efde41be7db6ffb0d334ef3
Johannes Koester <johannes.koester@uni-due.de>
2026-09-17 17:04:32 +0200

    fix

--- workflow/scripts/oncoprint.py
+++ workflow/scripts/oncoprint.py
@@ -178,8 +178,7 @@ def sort_oncoprint_labels(data):
 
             filtered_features = (
                 feature_matrix.sum(axis="index") >= snakemake.params.min_recurrence
-            ).reset_index()
-            breakpoint()
+            ).reset_index(drop=True)
 
             # filter to only those columns (we are transposed here)
             # with min_recurrence "True" values
