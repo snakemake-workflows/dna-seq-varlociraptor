b6ca5ae1e91ad478781d49c6c56f8cf213b3224b
Johannes Köster <johannes.koester@uni-due.de>
2026-09-22 12:51:45 +0200

    fix

--- workflow/scripts/process-call-tables.py
+++ workflow/scripts/process-call-tables.py
@@ -264,6 +264,5 @@ canonical_mane = canonical | mane_plus_clinical
 
 final_calls = calls[canonical_mane]
 final_calls = cleanup_dataframe(final_calls)
-breakpoint()
 write(final_calls, snakemake.output[0])
 # TODO add possibility to also see non-canoncical or non-mane+clinical transcripts (low priority, once everything else works).
